812018664b477de2095c15aa9470823ec67281b2
Diego Sebastian Markiewicz <dieghard@gmail.com>
2024-05-08 19:33:37 -0300

    modiicaciones hechas durante el curso

---
+++ CursoPython/CLASE3.py
@@ -0,0 +1,10 @@
+#Mostrar por pantalla la suma de dos variables.
+
+num1 = 1
+num2 = 3
+print(num1 + num2)
+
+
+
+resultado = num1 + num2
+print(resultado)
---
+++ CursoPython/clase3a.py
@@ -0,0 +1,2 @@
+apellido_Nombre, email, telefono = "Gaspar Oddovero", "gaspyoddovero742@gmail.com", "3385405049"
+print(apellido_Nombre, email, telefono)
