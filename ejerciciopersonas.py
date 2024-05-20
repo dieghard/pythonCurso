2dee8487f8c5064c4408dee4ad6ee55b014387b1
Diego Sebastian Markiewicz <dieghard@gmail.com>
2024-05-20 09:49:43 -0300

    se agrego el promedio de edad

--- ejerciciopersonas.py
+++ ejerciciopersonas.py
@@ -1,8 +1,13 @@
 #Datos persona 1
+suma_edad =  0
 nombre_apellido = input("Ingresa el nombre de la primer persona: ")
 direccion = input("Ingresa la direccion de la primer persona: ")
-edad = int(input("Ingresa la edad de la primer persona:"))
-
+edad = input("Ingresa la edad de la primer persona:")
+if not edad.isdigit()  :
+    print(f"Error: Debe ingresar una edad valida . fijate lo que pusiste!{
+        edad}")
+    exit
+suma_edad = suma_edad + edad
 
 nombre_mayor =  nombre_apellido
 direccion_mayor = direccion
@@ -17,7 +22,12 @@ posicion_menor_cargada = 1
 #Datos persona 2
 nombre_apellido= input("Ingresa el nombre de la segunda persona: ")
 direccion = input("Ingresa la direccion de la segunda persona: ")
-edad = int(input("Ingresa la edad de la segunda persona:"))
+edad = input("Ingresa la edad de la segunda persona:")
+if not edad.isdigit()  :
+    print(f"Error: Debe ingresar una edad valida . fijate lo que pusiste!{
+        edad}")
+    exit
+suma_edad = suma_edad + edad
 posicion_cargada = 2
 
 if edad > edad_mayor:
@@ -35,7 +45,13 @@ if edad < edad_menor:
 #Datos persona 3
 nombre_apellido= input("Ingresa el nombre de la tercera persona: ")
 direccion = input("Ingresa la direccion de la tercera persona: ")
-edad = int(input("Ingresa la edad de la tercera persona:"))
+edad = input("Ingresa la edad de la tercera persona:")
+if not edad.isdigit()  :
+    print(f"Error: Debe ingresar una edad valida . fijate lo que pusiste!{
+        edad}")
+    exit
+suma_edad = suma_edad + edad
+
 
 if edad > edad_mayor:
     nombre_mayor =  nombre_apellido
@@ -53,7 +69,13 @@ if edad < edad_menor:
 #Datos persona 4
 nombre_apellido= input("Ingresa el nombre de la cuarta persona: ")
 direccion = input("Ingresa la direccion de la cuarta persona: ")
-edad = int(input("Ingresa la edad de la cuarta persona:"))
+edad = input("Ingresa la edad de la cuarta persona:")
+if not edad.isdigit()  :
+    print(f"Error: Debe ingresar una edad valida . fijate lo que pusiste!{
+        edad}")
+    exit
+
+suma_edad = suma_edad + edad
 if edad > edad_mayor:
     nombre_mayor =  nombre_apellido
     direccion_mayor = direccion
@@ -65,5 +87,5 @@ if edad < edad_menor:
     edad_menor = edad
     posicion_menor_cargada = 4
 
-
-print(f"La persona con mayor edad es {nombre_mayor}, su direccion es{direccion_mayor} con {edad_mayor} años y fue cargada en la {posicion_mayor_cargada} y la persona de menor edad es {nombre_menor} cuya edad es {edad_menor} y vive en {direccion_menor} y fue cargado en la posición {posicion_menor_cargada}")
+promedio_edad = suma_edad / 4
+print(f"La persona con mayor edad es {nombre_mayor}, su direccion es{direccion_mayor} con {edad_mayor} años y fue cargada en la {posicion_mayor_cargada} y la persona de menor edad es {nombre_menor} cuya edad es {edad_menor} y vive en {direccion_menor} y fue cargado en la posición {posicion_menor_cargada} y el promedio de la edades ingresadas fueron {promedio_edad}")
