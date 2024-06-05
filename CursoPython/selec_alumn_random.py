6d20d1579d19607a9e0a28aa15c8f5701452b66a
Diego Sebastian Markiewicz <dieghard@gmail.com>
2024-06-05 12:55:20 -0300

    ejercicios hecho con los alumnos

--- CursoPython/selec_alumn_random.py
+++ CursoPython/selec_alumn_random.py
@@ -1,26 +1,29 @@
 import random
 
 # Listas de alumnos por grupo
+
+
 grupo_1 = [
-    "Pantaleo Patricio", "Caritá Maricel Ana", "Fauda Fermin", "Dominguez Paula",
+    "Agustin Abello", "Caritá Maricel Ana", "Fauda Fermin", "Dominguez Paula",
     "Mobilia Leticia", "Romero Mariana", "Carlos W Jimenez", "Miranda Florencia",
-    "Buffoni Monica", "Ana Toledo", "Ortiz Ana Belen", "Cochrane Guillermo",
+    "Buffoni Monica", "Ana Toledo", "Beltramone Maria Eugenia", "Cochrane Guillermo",
     "Javier Costamagna", "Shirley Janet Rodriguez", "Maria Andino",
-    "Jonathan Gallardo", "Leonardo Tomas Acosta", "Gonzalo Gimenez"
+    "Jonathan Gallardo", "Leonardo Tomas Acosta", "Gonzalo Gimenez" , "Gaspar Oddovero", "Diego Markiewicz"
 ]
+grupo_1_paso = ["Monica Buffoni", "Dominguez Paula", "Mobilia Leticia", "Beltramone Maria Eugenia"]
 
 grupo_2 = [
-    "Beltramone Maria Eugenia", "Dutto Santiago", "Daga Brisa", "Perna Emanuel",
+    "Dutto Santiago", "Daga Brisa", "Perna Emanuel",
     "Lorenzo Agustina", "Cristian Vincenti", "Pamela Gisela Izaguerre",
     "Marcelo Fenoglio", "Franco Ferreira Rodriguez", "Patricia Bosco",
     "Salvador Musso", "Pia Ferreira Rodriguez", "Sofia Dutto", "Franco Aguado",
-    "Camila Casali", "Agustin Abello"
+    "Camila Casali", "Caritá Maricel Ana"
 ]
-
+grupo_2_paso = ["Daga Brisa", "Sofia Dutto", "Caritá Maricel Ana"]
 # Seleccionar un nombre al azar de cada grupo
 alumno_azar_grupo_1 = random.choice(grupo_1)
 alumno_azar_grupo_2 = random.choice(grupo_2)
 
 # Imprimir los nombres seleccionados
-print(f"Alumno seleccionado al azar del Grupo 1: {alumno_azar_grupo_1}")
+#print(f"Alumno seleccionado al azar del Grupo 1: {alumno_azar_grupo_1}")
 print(f"Alumno seleccionado al azar del Grupo 2: {alumno_azar_grupo_2}")
--- CursoPython/tempCodeRunnerFile.py
+++ CursoPython/tempCodeRunnerFile.py
@@ -1,9 +1,5 @@
-import numpy as np
-
-# array = np.array([1, 2, 3, 4, 5])  # Crear un array a partir de una lista
-# array = np.arange(10)  # Crear un array con los números del 0 al 9
-# array = np.zeros(5)  # Crear un array de ceros con 5 elementos
-# array = np.ones(5)  # Crear un array de unos con 5 elementos
-array = np.linspace(0, 1, 10)  # Crear un array con 5 números espaciados uniformemente entre 0 y 1
-
-print (array)
+    numero=int(numero)
+        suma += numero
+        print(f"El número ingresado es: {numero} y la suma acumulada es: {suma}")
+    else:
+        print("No ingresaste un número:")
