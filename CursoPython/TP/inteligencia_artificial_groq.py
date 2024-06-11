a9d5f6df54d16cc6c75970a1ebd04171f1287d6b
Gaspar Oddovero Herrera <gaspyoddovero742@gmail.com>
2024-06-11 13:10:36 -0300

    consulta a una groq con python

---
+++ CursoPython/TP/inteligencia_artificial_groq.py
@@ -0,0 +1,28 @@
+#Recuerda usar pip install groq
+import os
+
+from groq import Groq
+
+client = Groq(
+    api_key="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",
+)
+while True:
+    print("Hola, a continuacion podras preguntarle lo que quieras a la inteligencia artificial")
+    
+    while True:
+        prompt = input("Ingrese lo que quieras preguntarle a la Inteligencia Artificial, si deseas salir puedes colocar la letra Q(quit): ").capitalize()
+        if prompt == "Q":
+            break
+        chat_completion = client.chat.completions.create(
+            messages=[
+                {
+                    "role": "user",
+                    "content": prompt,
+                }
+            ],
+            model="llama3-8b-8192",
+        )
+        print(chat_completion.choices[0].message.content)
+    flag = input("Si quieres dejar de usar la inteligencia artificial coloca SALIR, en caso contrario coloca cualquier cosa y volverás a comenzar:").upper()
+    if flag == "SALIR":
+        exit()
