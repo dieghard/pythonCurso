b0c2825f774dbb642fa00f3e77d61cbb7dca3a12
Diego Sebastian Markiewicz <dieghard@gmail.com>
2024-06-11 22:56:30 -0300

    Clase 14

--- CursoPython/TP/habla.py
+++ CursoPython/TP/habla.py
@@ -1,9 +1,6 @@
 '''
-pip pyttsx3
-pip  SpeechRecognition
-pip pyaudio
-pip openai-whisper
-pip soundfile
+pip install  pyttsx3
+pip install pyaudio
 
 '''
 import pyttsx3
@@ -14,24 +11,24 @@ voices = engine.getProperty('voices')
 voices = engine.getProperty('voices')
 
 # Muestra todas las voces disponibles
-#for i, voice in enumerate(voices):
-   # print(f"{i}. {voice.name} ({voice.languages})")
+for i, voice in enumerate(voices):
+    print(f"{i}. {voice.name} ({voice.languages})")
 
 input ("¿Saludar a los alumnos??? (S/N)")
-# Obtén el volumen actual
+#  volumen actual
 volume = engine.getProperty('volume')
 #print(f"Volumen actual: {volume}")
-# Obtén la velocidad actual
+# velocidad actual
 rate = engine.getProperty('rate')
 #print(f"Velocidad actual: {rate}")
-# Cambia la velocidad a 150
+# Cambia velocidad a 150
 engine.setProperty('rate', 150)
-#print("La velocidad se ha cambiado a 150")
+#print("La velocidad cambio a 150")
 
 engine.setProperty('voice', voices[0].id)
-engine.say("¿Hola Queridos Alumnos como andan??")
-engine.say("Esta noche vamos a disfrutar la velada con los dos profesores mas zarpados en el mundo de la programacion")
-engine.say("Claro que si!!!! son Diego y Gaspar")
-engine.say("y les van a volar el cerebro.... puuuuuuffffff... NOS VEMOS")
+#engine.say("¿Hola Queridos Alumnos como andan??")
+#engine.say("Esta noche vamos a disfrutar la velada con los dos profesores mas zarpados en el mundo de la programacion")
+#engine.say("Claro que si!!!! son Diego y Gaspar")
+#engine.say("y les van a volar el cerebro.... puuuuuuffffff... NOS VEMOS")
 
 engine.runAndWait()
