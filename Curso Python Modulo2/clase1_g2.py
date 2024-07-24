43dff6578deb2c4f6befee532a0fcc21afa0651c
GOddovero <gaspyoddovero742@gmail.com>
2024-07-24 08:09:14 -0300

    clase1_modulo2

---
+++ Curso Python Modulo2/clase1_g2.py
@@ -0,0 +1,28 @@
+def operacion():
+    numero_1 = int(input("Ingrese el numero a utilizar:"))
+    numero_2 = int(input("Ingrese el numero a utilizar:"))
+    operacion = input("Ingrese la operacion a realizar(+,-,*,/,\%\ o suma , resta,multiplicacion , division, modulo)")
+
+    if operacion == "+" or operacion == "suma":
+        resultado = numero_1 + numero_2
+    elif operacion == "-" or operacion == "resta":
+        resultado = numero_1 - numero_2
+    elif operacion == "*" or operacion == "multiplicacion":
+        resultado = numero_1 * numero_2
+    elif operacion == "/" or operacion == "division":
+        resultado = numero_1 / numero_2
+    elif operacion == "%" or operacion == "modulo":
+        resultado = numero_1 % numero_2    
+    else:
+        print("Escribiste cualquier cosa, menos una operacion")
+    return resultado
+
+#Funcion para imprimir un texto:
+
+
+def imprimir(parametro):
+    print(parametro)
+    return
+
+imprimir(input("Ingrese texto:"))
+imprimir("Hola Mundo")
